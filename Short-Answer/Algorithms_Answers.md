50ca7d8b52421c8f84957489ed75a5b1ea4937c2
MrR3set <bairon.pazrobles@gmail.com>
2020-07-24 10:46:16 -0700

    mvp

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,12 @@
 
 ## Exercise I
 
-a)
+a) 0(1) as the input will not be changing
 
+b) 0(n) as the input increases the runtime will be longer as it takes mode elements to go trough
 
-b)
-
-
-c)
+c) 0(1) as it will only add 2 to the value.
 
 ## Exercise II
 
-
+If we use a BTS we can determinate, according to how the egg acts, if we are higher or lower than f (return 1). If the egg is broken we must search from the left of the three until the egg wont be broken returning n which would be f. Alternatively if the egg is not broken we must go to the right of the three until the egg breaks-
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,18 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    if len(word) == 0:
+        return 0
+
+    # If the word starts with T check what the second letter is
+    if word[0] == "t":
+        if len(word) == 1:
+            return 0  # If the word has len 1 return 0
+
+        if word[1]=="h":
+            return 1+count_th(word[2:]) # If the second letter is h then add 1 to the count and call count_th with the rest of the stuff
+        else:
+            return 0+count_th(word[1:])
+
+    return count_th(word[1:])
+
